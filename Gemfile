5006b08107d94760295aa02456d66205597dbec0
Hakimu Jackson <hakimu@newrelic.com>
2015-06-01 17:37:22 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'unicorn'
+gem 'sinatra'
+gem 'data_mapper'
+gem 'newrelic_rpm'
---
+++ app.rb
@@ -0,0 +1,32 @@
+require 'json'
+require 'unicorn'
+require 'sinatra'
+require 'data_mapper'
+require 'newrelic_rpm'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/notecard.db")
+
+class NoteCard
+	include DataMapper::Resource
+	property :id, Serial
+	property :term, Text, :required => true
+	property :definition, Text, :required => true	
+	property :complete, Boolean, :required => true, :default => false
+	property :created_at, DateTime
+	property :updated_at, DateTime
+end
+
+DataMapper.finalize.auto_upgrade!
+
+# note = NoteCard.new term: "Rspec", definition: "A behavior-driven development (BDD) framework for Ruby", created_at: Time.now
+# note.save
+
+get '/' do
+	@notecards = NoteCard.all :order => :id.desc
+	erb :home
+end
+
+
+
+
+
